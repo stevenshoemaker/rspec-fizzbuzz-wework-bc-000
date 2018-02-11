ab598e70f03b7ad1b1f82061a4e9420e4e5af38c
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-11 23:52:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 5 == 0 # if the number int is divisible by 3
+  if int % 3 == 5 && int % 5 == 3
+    "Fizzbuzz"
+  elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
     elsif int % 3 == 0
     "Fizz"
-    elsif int % 3 == 5 && int % 5 == 3
-    "Fizzbuzz"
   end
 end
