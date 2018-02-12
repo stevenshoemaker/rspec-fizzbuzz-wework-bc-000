ba5964f480f60405de391f10ad752b57084089a0
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 00:16:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
-    "Fizz" 
+  if int !(% 5 == 0 && int % 3 == 0)
+    "Fizzbuzz" 
     elsif int % 5 == 0
     "Buzz"
     elsif int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"
+    "Fizz"
   end
 end
